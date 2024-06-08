2cfe48edcc6454e90d3e537a90788a1dbd619b99
rajdev04 <rajdevshrivastava72@gmail.com>
2024-06-08 15:44:06 +0530

    setup compelete

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const userModel = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use(cookieParser());
+
+app.get('/',function(req,res,next){
+    res.render('index');
+})
+
+app.listen(3000);
