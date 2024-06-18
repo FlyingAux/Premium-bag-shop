834e7ca79cd9b2e3a9ce465c7a3df7b3c7f107eb
rajdev04 <rajdevshrivastava72@gmail.com>
2024-06-18 15:08:21 +0530

    cart needs improvement in UI

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const isLoggedIn = require('../middlewares/isLoggedIn');
 const productModel = require('../models/product');
 const userModel = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 router.get('/',function(req,res,next){
     let error = req.flash('error');
@@ -13,18 +14,33 @@ router.get('/',function(req,res,next){
 router.get('/shop',isLoggedIn,async function(req,res,next){
     let products = await productModel.find()
     res.render('shop',{products});
+    console.log(products)
 })
 
 
-// router.get('/cart/:productid',async function(req,res,next){
-//     let email = req.body;
-//     let user = await userModel.findOne({email: email});
-//     console.log(user)
-// })
+router.get('/cart/:pid',async function(req,res,next){
+    let decoded = jwt.verify(req.cookies.token,process.env.JWT_KEY);
+    let user = await userModel.findOne({email: decoded.email}).select("-password");
+
+    let product = await productModel.find({_id: req.params.pid});
+
+    if(user.cart.indexOf(product._id) !== -1){
+        let index1 = user.cart.indexOf(product._id);
+        user.cart.splice(index1 , 1);
+    }
+    else{
+        user.cart.push(product._id);
+    }
+    await user.save();
+    res.redirect('/cart');
+})
 
 router.get('/cart',isLoggedIn,async function(req,res,next){
-    res.render('cart');
-})
+    let decoded = jwt.verify(req.cookies.token,process.env.JWT_KEY);
+    let user = await userModel.findOne({email: decoded.email}).select('-password').populate('cart')
+    console.log(user)
+    res.render('cart',{user});
+});
 
 
 module.exports = router;
